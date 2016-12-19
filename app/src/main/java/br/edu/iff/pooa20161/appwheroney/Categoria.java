8443d802cce9c054ed826acba42e752f95a8b39f
JehQueiroz <jessicasq1@hotmail.com>
2016-12-18 23:01:07 -0200

    Classe Categoria

---
+++ app/src/main/java/br/edu/iff/pooa20161/appwheroney/Categoria.java
@@ -0,0 +1,28 @@
+package br.edu.iff.pooa20161.appwheroney;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Categoria extends AppCompatActivity {
+    Button btAdicionar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categoria);
+
+        btAdicionar=(Button)findViewById(R.id.btAdicionar);
+
+        btAdicionar.setOnClickListener(new View.OnClickListener(){
+          @Override
+          public void onClick(View v){
+                  Intent cat = new Intent(Categoria.this, Wheroney.class);
+              startActivity(cat);
+              finish();
+         }
+         });
+
+    }
+}

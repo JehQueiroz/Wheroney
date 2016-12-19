fffd2e5abf5dd64808950e2263ea71ab2e25ef56
JehQueiroz <jessicasq1@hotmail.com>
2016-12-18 23:00:44 -0200

    Classe Cadastrar Conta
    Cadastro da Conta do Usuário

--- app/src/main/java/br/edu/iff/pooa20161/appwheroney/CadastrarConta.java
+++ app/src/main/java/br/edu/iff/pooa20161/appwheroney/CadastrarConta.java
@@ -1,5 +1,20 @@
+package br.edu.iff.pooa20161.appwheroney;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CadastrarConta extends AppCompatActivity {
      Button btSalvar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_conta);
+
         btSalvar=(Button)findViewById(R.id.btSalvar);
+
           btSalvar.setOnClickListener(new View.OnClickListener(){
          @Override
         public void onClick(View v){
@@ -8,3 +23,5 @@
             finish();
                  }
           });
+    }
+}

f5b9a34719ddd014ca3d568dc91bc4bb0e6383af
JehQueiroz <jessicasq1@hotmail.com>
2016-12-18 23:01:35 -0200

    Classe Lançamentos

---
+++ app/src/main/java/br/edu/iff/pooa20161/appwheroney/Lancamentos.java
@@ -0,0 +1,13 @@
+package br.edu.iff.pooa20161.appwheroney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Lancamentos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lancamentos);
+    }
+}
